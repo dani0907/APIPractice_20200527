8fd01987146f3c760ae38e2b183df0621d55c966
dy696786 <dy696786@gmail.com>
2020-05-27 16:35:36 +0900

    서버에 id/pw를 담아서 로그인 실제 시도.   => 응답이 돌아옴을 확인.   => 이를 활용한 반영 조치 x

---
+++ app/src/main/java/com/example/apipractice_20200527/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.example.apipractice_20200527;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+    public Context mContxt = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+}
